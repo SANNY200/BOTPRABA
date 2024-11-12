334d7842a937eabfbf745e99ab2473a2c409b659
SANNY200 <sannyofficial2001@gmail.com>
2024-11-12 21:14:27 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -36,8 +36,4 @@ module.exports = {
     GITHUB_USERNAME: username,
     GITHUB_AUTH_TOKEN: GITHUB_TOKEN,
 };
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
